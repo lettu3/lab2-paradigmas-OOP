27050d68f949bfbeb22d56ce56a3561f8d0aa990
ferndz <nafernandez@mi.unc.edu.ar>
2024-05-03 21:29:55 -0300

    estructura básica de la clase Article

--- src/feed/Article.java
+++ src/feed/Article.java
@@ -1,5 +1,38 @@
 package feed;
 
 public class Article {
-    // TODO
-}
+    
+    private String title;
+    private String description;
+    private String pubDate;
+    private String link;
+
+    public Article(String title, String description, String pubDate, String link) {
+        this.title = title;
+        this.description = description;
+        this.pubDate = pubDate;
+        this.link = link;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+    public String getLink(){
+        return link;
+    }
+
+    public void printArticle(){
+        System.out.println("Title: " + title);
+        System.out.println("Description: " + description);
+        System.out.println("Publicationn Date: " + pubDate);
+        System.out.println("Link:\n" + title);
+    }
+}   
