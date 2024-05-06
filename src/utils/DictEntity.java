e5e0a24894b024b39b2653dcf1534e6c28875b0a
Ignacio <ignacio.gomez12@mi.unc.edu.ar>
2024-05-06 11:35:01 -0300

    se expande el tipo diccionario de entidades, faltan completar sus funciones

--- src/utils/DictEntity.java
+++ src/utils/DictEntity.java
@@ -4,13 +4,89 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DictEntity {
+
+    private class Entity {
+        private String label;
+        private List<String> keywords;
+        private String category;
+        private List<String> topics;
+
+    
+        //constructor
+        public Entity(String label, String category, List<String> topics, List<String> keywords) {
+            this.label = label;
+            this.keywords = keywords;
+            this.category = category;
+            this.topics = topics;
+        }
+
+        //getters
+        public String getLabel() {
+            return label;
+        }
+
+        public List<String> getKeywords() {
+            return keywords;
+        }
+
+        public String getCategory() {
+            return category;
+        }
+
+        public List<String> topics (){
+            return topics;
+        }
+
+    }
+
+    
+    public Entity<List> dictionary;
+
+    public DictEntity(){
+        dictionary = new ArrayList<>();
+    }
+
+    public addToDictEntity(String label, String category, List<String> topics, List<String> keywords){
+        dictionary.add(new Entity(label, category, topics, keywords));
+    }
+    
+    public bool DictContainsLabel (String label){
+        //completar
+    }
+
+
+    //PRE: DictContainsLabel(label)
+    
+    //toma una label, y devuelve la categoria de esa entidad si es que existe
+    public String getCategoryfromlabel (String label){
+
+    }
+
+    //toma una label y devuelve el topico de la entidad si es que existe
+    public List<String> getTopicFromLabel (String label){
+
+    }
+
+    //toma una label y devuelve la lista de keywords de la entidad si es que existe
+    public List<String> getKeywordsFromLabel (String label){
+
+    }
+
+    //toma una keyword y devuelve la label asociada
+    public String getLabelFromKeyword (String keyword){
+
+    }
+}
+
+public class Entity {
     private String label;
     private List<String> keywords;
     private String category;
-    private List<String> topics; 
+    private List<String> topics;
 
+    
     //constructor
-    public DictEntity(String label, String category, List<String> topics, List<String> keywords) {
+    public Entity(String label, String category, List<String> topics, List<String> keywords) {
         this.label = label;
         this.keywords = keywords;
         this.category = category;
@@ -34,5 +110,4 @@ public class DictEntity {
         return topics;
     }
 
-    //
 }
