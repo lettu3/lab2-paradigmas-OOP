cea61e50349203e2a351fea1c84f2aca10ef8df9
ferndz <nafernandez@mi.unc.edu.ar>
2024-05-06 18:50:04 -0300

    Implementación básica(no optimizada) de DictEntity

--- src/utils/DictEntity.java
+++ src/utils/DictEntity.java
@@ -33,25 +33,29 @@ public class DictEntity {
             return category;
         }
 
-        public List<String> topics (){
+        public List<String> getTopics (){
             return topics;
         }
 
     }
 
-    
-    public Entity<List> dictionary;
+    private List<Entity> dictionary;
 
     public DictEntity(){
         dictionary = new ArrayList<>();
     }
 
-    public addToDictEntity(String label, String category, List<String> topics, List<String> keywords){
+    public void addToDictEntity(String label, String category, List<String> topics, List<String> keywords){
         dictionary.add(new Entity(label, category, topics, keywords));
     }
     
-    public bool DictContainsLabel (String label){
-        //completar
+    public boolean DictContainsLabel (String label){
+        for(Entity entity : dictionary) {
+            if(entity.getLabel().equals(label)){
+                return true;
+            }
+        }
+        return false;
     }
 
 
@@ -59,55 +63,43 @@ public class DictEntity {
     
     //toma una label, y devuelve la categoria de esa entidad si es que existe
     public String getCategoryfromlabel (String label){
-        //completar
+        for(Entity entity : dictionary) {
+            if(entity.getLabel().equals(label)){
+                return entity.getCategory();
+            }
+        }
+        return null;
     }
 
     //toma una label y devuelve el topico de la entidad si es que existe
     public List<String> getTopicFromLabel (String label){
-        //completar
+        for(Entity entity : dictionary) {
+            if(entity.getLabel().equals(label)){
+                return entity.getTopics();
+            }
+        }
+        return null;
     }
 
     //toma una label y devuelve la lista de keywords de la entidad si es que existe
     public List<String> getKeywordsFromLabel (String label){
-        //completar
+        for(Entity entity : dictionary) {
+            if(entity.getLabel().equals(label)){
+                return entity.getKeywords();
+            }
+        }
+        return null;
     }
 
     //toma una keyword y devuelve la label asociada
     public String getLabelFromKeyword (String keyword){
-        //completar
+        for(Entity entity : dictionary) {
+            for(String key : entity.getKeywords()){
+                if(key.equals(keyword)){
+                    return entity.getLabel();
+                }
+            }
+        }
+        return null;
     }
-}
-
-public class Entity {
-    private String label;
-    private List<String> keywords;
-    private String category;
-    private List<String> topics;
-
-    
-    //constructor
-    public Entity(String label, String category, List<String> topics, List<String> keywords) {
-        this.label = label;
-        this.keywords = keywords;
-        this.category = category;
-        this.topics = topics;
-    }
-
-    //getters
-    public String getLabel() {
-        return label;
-    }
-
-    public List<String> getKeywords() {
-        return keywords;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public List<String> topics (){
-        return topics;
-    }
-
 }
